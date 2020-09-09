ea4ced193df846cc907956a3c67b370d1fd71d68
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-09 19:16:52 +0000

    Deploy Erriez/ErriezDS3231 to github.com/Erriez/ErriezDS3231.git:gh-pages

--- search/all_5.js
+++ search/all_5.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['getagingoffset',['getAgingOffset',['../class_erriez_d_s3231.html#a34e80a1e8279139e85e60b5852197076',1,'ErriezDS3231']]],
   ['getalarmflag',['getAlarmFlag',['../class_erriez_d_s3231.html#ad41fe80dbb5eafa734656bab103847bb',1,'ErriezDS3231']]],
+  ['getdatetime',['getDateTime',['../class_erriez_d_s3231.html#a5c4f30f85b325e4bcdecd976cfda0f93',1,'ErriezDS3231']]],
   ['getepoch',['getEpoch',['../class_erriez_d_s3231.html#a05a1fab6a5d852c6b7e7749754cc7a63',1,'ErriezDS3231']]],
   ['gettemperature',['getTemperature',['../class_erriez_d_s3231.html#a57de666506f26c9524ef1a7a2aff23ad',1,'ErriezDS3231']]],
   ['gettime',['getTime',['../class_erriez_d_s3231.html#abaa1d99e3954802c1be37e9dfd585ba9',1,'ErriezDS3231']]]
